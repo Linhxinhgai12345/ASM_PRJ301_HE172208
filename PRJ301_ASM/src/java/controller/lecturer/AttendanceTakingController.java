984f377418c985510dbc2508c4bcc037c8f3d0ed
Linhxinhgai12345 <dieulinh06082003@gmail.com>
2024-03-03 12:00:30 +0700

    fixed attendancetsasasasasassa
    fix for better, have bug

--- PRJ301_ASM/src/java/controller/lecturer/AttendanceTakingController.java
+++ PRJ301_ASM/src/java/controller/lecturer/AttendanceTakingController.java
@@ -5,12 +5,18 @@
 
 package controller.lecturer;
 
+import dal.LessionDBContext;
+import entity.Account;
+import entity.Attendence;
+import entity.Lession;
+import entity.Student;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 /**
  *
@@ -22,16 +28,20 @@ public class AttendanceTakingController extends HttpServlet {
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /** 
      * Handles the HTTP <code>GET</code> method.
+     * @param req
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-
-    } 
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp, Account account) throws ServletException, IOException {
+        int leid = Integer.parseInt(req.getParameter("id"));
+        LessionDBContext db = new LessionDBContext();
+        ArrayList<Attendence> atts = db.getAttendencesByLession(leid);
+        req.setAttribute("atts", atts);
+        req.getRequestDispatcher("../view/lecturer/att.jsp").forward(req, resp);
+    
+    }
 
     /** 
      * Handles the HTTP <code>POST</code> method.
@@ -41,9 +51,25 @@ public class AttendanceTakingController extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
     throws ServletException, IOException {
-
+        int leid = Integer.parseInt(req.getParameter("id"));
+        LessionDBContext db = new LessionDBContext();
+        ArrayList<Student> students = db.getStudentsByLession(leid);
+        ArrayList<Attendence> atts = new ArrayList<>();
+        Lession lession = new Lession();
+        lession.setId(leid);
+        for (Student student : students) {
+            Attendence a = new Attendence();
+            a.setLession(lession);
+            a.setStudent(student);
+            a.setDescription(req.getParameter("description"+student.getId()));
+            a.setPresent(req.getParameter("present"+student.getId()).equals("yes"));
+            atts.add(a);
+        }
+        db.takeAttendances(leid, atts);
+        resp.sendRedirect("att?id="+leid);
+    
     }
 
     /** 
